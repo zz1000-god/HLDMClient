6a18bb1064ba1a0d8f0df172e4ce29611043c3c6
zz1000-god <maksmine353@gmail.com>
2025-07-23 09:16:43 +0300

    Added old scoreboard like in OpenAG

---
+++ cl_dll/hud_oldscoreboard.h
@@ -0,0 +1,33 @@
+#ifndef OLDSCOREBOARD_H
+#define OLDSCOREBOARD_H
+#pragma once
+
+#include "cl_entity.h"
+#include "interpolation.h"
+
+class CHudOldScoreboard: public CHudBase
+{
+public:
+	int Init( void );
+	int VidInit( void );
+	int Draw( float flTime );
+	void Reset( void );
+
+	bool IsVisible( );
+	void ShowScoreboard( bool bShow );
+private:
+	typedef struct
+	{
+		HSPRITE spr;
+		wrect_t rc;
+	} icon_flagstatus_t;
+
+	icon_flagstatus_t m_IconFlagScore;
+
+	cvar_t *m_pCvarOldScoreboardWidth;
+	cvar_t *m_pCvarOldScoreboardColorsTags;
+
+	float m_WidthScale;
+};
+
+#endif //OLD_SCOREBOARD_H
